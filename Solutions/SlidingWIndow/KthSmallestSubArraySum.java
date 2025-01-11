09224e6137a89dc9959d6c14d6e69366e6e4a5bd
abhishekmano <abhishekmanoharan1999@gmail.com>
2025-01-11 10:49:29 +0530

    Code Formatting changes

--- Solutions/SlidingWIndow/KthSmallestSubArraySum.java
+++ Solutions/SlidingWIndow/KthSmallestSubArraySum.java
@@ -1,42 +1,43 @@
 package Solutions.SlidingWIndow;
 
 import java.util.Arrays;
+
 //https://leetcode.com/problems/kth-smallest-subarray-sum/
 // 1918. Kth Smallest Subarray Sum
 class Solution {
-    //Binary Search on Sum less than K
-    //Runs in NLogS where S is the Sum
+    // Binary Search on Sum less than K
+    // Runs in NLogS where S is the Sum
     public int kthSmallestSubarraySum(int[] nums, int k) {
         int left = 0;
         int right = Arrays.stream(nums).sum();
         int result = 0;
-        while(left <= right){
-            int mid = left + (right - left)/2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             int count = subArrayWithSumLess(nums, mid);
-            if(count < k){
+            if (count < k) {
                 result = mid;
                 left = mid + 1;
-            }
-            else{
-                right = mid -1;
+            } else {
+                right = mid - 1;
             }
         }
         return result;
-        
+
     }
-    //Runs in O(n) Sliding Window
-    public int subArrayWithSumLess(int[] nums , int sum){
+
+    // Runs in O(n) Sliding Window
+    public int subArrayWithSumLess(int[] nums, int sum) {
         int count = 0;
         int left = 0;
-        int running = 0 ;
+        int running = 0;
         int n = nums.length;
-        for(int right = 0 ; right < n ; ++right){
+        for (int right = 0; right < n; ++right) {
             running += nums[right];
-            while(running >= sum){
+            while (running >= sum) {
                 running -= nums[left];
                 left++;
             }
-            count += (right - left +1);
+            count += (right - left + 1);
         }
         return count;
     }
--- Solutions/Stack/MonotonicStack/SumOfSubArrayMinimum.java
+++ Solutions/Stack/MonotonicStack/SumOfSubArrayMinimum.java
@@ -2,34 +2,39 @@ package Solutions.Stack.MonotonicStack;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+
 //907. Sum of Subarray Minimums
 //https://leetcode.com/problems/sum-of-subarray-minimums
 public class SumOfSubArrayMinimum {
     public int sumSubarrayMins(int[] arr) {
-        int mod =1_000_000_007;
+        int mod = 1_000_000_007;
         int n = arr.length;
         long res = 0;
         Deque<Pair> mono = new ArrayDeque<Pair>();
-        //for every element We have to find smallest element on both side that limits the range of current element to be minimum
-        //Every element popped from stack the top element in stack is left min and current is right min
-        for(int idx = 0 ; idx <= n ; ++idx){
-            while(!mono.isEmpty() && (idx == n || arr[idx] <= mono.peek().num)){
+        // for every element We have to find smallest element on both side that limits
+        // the range of current element to be minimum
+        // Every element popped from stack the top element in stack is left min and
+        // current is right min
+        for (int idx = 0; idx <= n; ++idx) {
+            while (!mono.isEmpty() && (idx == n || arr[idx] <= mono.peek().num)) {
                 var top = mono.pop();
                 int mid = top.index;
                 int left = -1;
-                if(!mono.isEmpty()){
+                if (!mono.isEmpty()) {
                     left = mono.peek().index;
                 }
                 int right = idx;
-                long count = (right - mid ) * (mid - left);
+                long count = (right - mid) * (mid - left);
                 count = count % mod;
-                res +=  (count * top.num)%mod;
+                res += (count * top.num) % mod;
                 res = res % mod;
             }
-            if(idx != n)
-            mono.push(new Pair(arr[idx], idx));
+            if (idx != n)
+                mono.push(new Pair(arr[idx], idx));
         }
-        return (int)res;
+        return (int) res;
     }
-    public record Pair(int num , int index){};
+
+    public record Pair(int num, int index) {
+    };
 }
