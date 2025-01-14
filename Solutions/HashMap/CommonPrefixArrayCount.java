0cfe2c2d686fc17be0eac401daa28c648eeeb09d
abhishekmano <abhishekmanoharan1999@gmail.com>
2025-01-14 10:06:59 +0530

    Daily Problem Jan 14 Added

---
+++ Solutions/HashMap/CommonPrefixArrayCount.java
@@ -0,0 +1,24 @@
+package Solutions.HashMap;
+
+//2657. Find the Prefix Common Array of Two Arrays
+//https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays
+public class CommonPrefixArrayCount {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n = A.length;
+        int[] freq = new int[n + 1];
+        int[] result = new int[n];
+        for (int idx = 0; idx < n; ++idx) {
+            if (idx != 0)
+                result[idx] = result[idx - 1];
+            freq[A[idx]]++;
+            freq[B[idx]]++;
+            if (freq[A[idx]] == 2)
+                result[idx]++;
+            // This is to avoid double counting
+            if (A[idx] != B[idx])
+                if (freq[B[idx]] == 2)
+                    result[idx]++;
+        }
+        return result;
+    }
+}
