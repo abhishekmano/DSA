18072dae850baeb79b8313354115f96c000e66cb
abhishekmano <abhishekmanoharan1999@gmail.com>
2025-02-07 00:47:21 +0530

    Added Kruskal algorithm and one q with MST

---
+++ Solutions/Graph/MinimumSpanningTree/ConnectingCities.java
@@ -0,0 +1,58 @@
+package Solutions.Graph.MinimumSpanningTree;
+
+import java.util.Arrays;
+
+//1135. Connecting Cities With Minimum Cost
+//https://leetcode.com/problems/connecting-cities-with-minimum-cost
+public class ConnectingCities {
+    public int minimumCost(int n, int[][] connections) {
+        int sum = 0;
+        int count = 0;
+        UnionFind set = new UnionFind(n + 1);
+        Arrays.sort(connections, (a, b) -> a[2] - b[2]);
+        for (int[] edge : connections) {
+            if (set.union(edge[0], edge[1])) {
+                sum += edge[2];
+                count++;
+                if (count == n - 1)
+                    return sum;
+            }
+        }
+        return -1;
+    }
+
+    public class UnionFind {
+        int[] parent;
+        int[] size;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for (int idx = 0; idx < n; ++idx) {
+                size[idx] = 1;
+                parent[idx] = idx;
+            }
+        }
+
+        public int findParent(int a) {
+            if (parent[a] == a)
+                return a;
+            return parent[a] = findParent(parent[a]);
+        }
+
+        public boolean union(int a, int b) {
+            int pa = findParent(a);
+            int pb = findParent(b);
+            if (pa == pb)
+                return false;
+            if (size[pa] > size[pb]) {
+                size[pa] += size[pb];
+                parent[pb] = pa;
+            } else {
+                size[pb] += size[pa];
+                parent[pa] = pb;
+            }
+            return true;
+        }
+    }
+}
